afea3c5f0bdc6266cec9a4380ec8fc4a56283007
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2024-04-27 16:31:23 -0500

    cleanup

--- main.c
+++ main.c
@@ -8,6 +8,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+enum {
+	BUFFER_SIZE = 4096
+};
+
+struct opts {
+	char **filenames;
+	long tab_width;
+	int max_col_only;
+	long alert;
+};
+
+struct line_info {
+	long len;
+	long lineno;
+	long longest_len;
+	long longest_lineno;
+};
+
 static void print_usage(void) {
 	printf(
 		"linelen: Get length of each line in a file.\n"
@@ -23,13 +41,6 @@ static void print_usage(void) {
 	);
 }
 
-struct opts {
-	char **filenames;
-	long tab_width;
-	int max_col_only;
-	long alert;
-};
-
 static void parse_opts(
 	int argc,
 	char **argv,
@@ -42,20 +53,20 @@ static void parse_opts(
 	opts->max_col_only = 0;
 	opts->alert = -1;
 
-	while ((c = getopt(argc, argv, "t:a:hm")) != -1) {
+	while ((c = getopt(argc, argv, "a:hmt:")) != -1) {
 		switch (c) {
 		case 'a':
 			opts->alert = atol(optarg);
 			break;
-		case 't':
-			opts->tab_width = atol(optarg);
-			break;
-		case 'm':
-			opts->max_col_only = 1;
-			break;
 		case 'h':
 			print_usage();
 			exit(EXIT_SUCCESS);
+		case 'm':
+			opts->max_col_only = 1;
+			break;
+		case 't':
+			opts->tab_width = atol(optarg);
+			break;
 		default:
 			print_usage();
 			exit(EXIT_FAILURE);
@@ -70,49 +81,38 @@ static void parse_opts(
 	opts->filenames = argv + optind;
 }
 
-static void print_info(long len, long lineno, const char *filename) {
-	printf("%s:%ld\t%ld\n", filename, lineno, len);
-}
-
-enum {
-	BUFFER_SIZE = 4096
-};
-
 static void handle_character(
 	char c,
 	const char *filename,
-	long *len,
-	long *lineno,
-	long *longest_len,
-	long *longest_lineno,
-	const struct opts *opts
+	const struct opts *opts,
+	struct line_info *li /* out */
 ) {
 	int always_print = (opts->alert < 0) && (!opts->max_col_only);
 	switch (c) {
 	case '\n':
-		if ((opts->alert >= 0) && (*len > opts->alert)) {
-			print_info(*len, *lineno, filename);
+		if ((opts->alert >= 0) && (li->len > opts->alert)) {
+			printf("%s:%ld\t%ld\n", filename, li->lineno, li->len);
 		}
 
-		if (opts->max_col_only && (*len > *longest_len)) {
-			*longest_lineno = *lineno;
-			*longest_len = *len;
+		if (opts->max_col_only && (li->len > li->longest_len)) {
+			li->longest_lineno = li->lineno;
+			li->longest_len = li->len;
 		}
 
 		if (always_print) {
-			print_info(*len, *lineno, filename);
+			printf("%s:%ld\t%ld\n", filename, li->lineno, li->len);
 		}
 
-		*len = 0;
-		*lineno = *lineno + 1;
+		li->len = 0;
+		li->lineno++;
 		break;
 	case '\t':
-		*len += opts->tab_width;
+		li->len += opts->tab_width;
 		break;
 	case '\r':
 		break;
 	default:
-		*len += 1;
+		li->len++;
 		break;
 	}
 }
@@ -120,12 +120,14 @@ static void handle_character(
 static int run_file(const char *filename, const struct opts *opts) {
 	FILE *fp = NULL;
 	int rc = 1;
-	long lineno = 1;
-	long len = 0;
-	long longest_lineno = 1;
-	long longest_len = 0;
 	size_t bytes_read = 0;
 	char buf[BUFFER_SIZE + 1];
+	struct line_info li;
+
+	li.lineno = 1;
+	li.len = 0;
+	li.longest_lineno = 1;
+	li.longest_len = 0;
 
 	fp = fopen(filename, "r");
 	if (!fp) {
@@ -147,11 +149,8 @@ static int run_file(const char *filename, const struct opts *opts) {
 			handle_character(
 				buf[i],
 				filename,
-				&len,
-				&lineno,
-				&longest_len,
-				&longest_lineno,
-				opts
+				opts,
+				&li
 			);
 		}
 
@@ -161,7 +160,12 @@ static int run_file(const char *filename, const struct opts *opts) {
 	}
 
 	if (opts->max_col_only) {
-		print_info(longest_len, longest_lineno, filename);
+		printf(
+			"%s:%ld\t%ld\n",
+			filename,
+			li.longest_lineno,
+			li.longest_len
+		);
 	}
 	rc = 0;
 done:
